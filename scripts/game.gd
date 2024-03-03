49dc3052ee5d458e203ee31f07c909971ded2370
hkerstyn <>
2024-03-03 15:53:18 +0100

    changed example dialog

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,8 +3,11 @@ class_name Game
 
 func _ready():
 	SceneLoader.load_location("Forest", "Spawn")
-	spam()
 
-func spam():
-	while true:
-		await $UI/Terminal.print("Hi, this is a test")
+func _process(_delta):
+	if Input.is_action_just_pressed("test"):
+		await $UI/Terminal.print("Sorry, I'm freshly out of fennel.")
+		await $UI/Terminal.print("My guy didn't send any this week.")
+		await $UI/Terminal.print("Maybe try going to him directly?")
+		await $UI/Terminal.print("He lives up north in Dominion.")
+		await $UI/Terminal.new_page()
